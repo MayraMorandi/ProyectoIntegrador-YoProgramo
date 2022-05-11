3b7e4269b474d38d3f6c4d4c3f3e939c1426a28c
Mayra <mayra_morandi@hotmail.com>
2022-05-11 18:58:38 -0300

    FrontEnd
    Se sube el Front End del proyecto

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoArgProComponent } from './componentes/logo-arg-pro/logo-arg-pro.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HysComponent } from './componentes/hys/hys.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoArgProComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectosComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
